ace9b4781e50ba68c7163da92ffeab7814761738
Andrew R. Lake <andy@es.net>
2026-01-13 13:20:35 -0500

    Remote ip cache files (#44)
    * Adding support for downloading trie files over HTTP
    
    * Updating unit tests

--- metranova/cachers/ip.py
+++ metranova/cachers/ip.py
@@ -12,6 +12,7 @@ class IPCacher(BaseCacher):
     def __init__(self):
         super().__init__()
         self.logger = logger
+        self.base_url = os.getenv('IP_CACHER_BASE_URL', None)
         self.cache_dir = os.getenv('IP_CACHER_DIR', 'caches')
         self.cache_refresh_interval = int(os.getenv('IP_CACHER_REFRESH_INTERVAL', '600'))
         #tables to load
@@ -24,20 +25,43 @@ class IPCacher(BaseCacher):
 
     def prime(self):
         for table in self.tables:
-            filename = os.path.join(self.cache_dir, f"ip_trie_{table}.pickle")
-            if os.path.exists(filename):
-                self.logger.info(f"Found IP trie cache file for table {table}: {filename}")
+            if self.base_url:
+                #load from remote URL
+               self.load_from_url(table)
+            elif self.cache_dir:
+                #load from local file
+                self.load_from_file(table)
             else:
-                self.logger.warning(f"No IP trie cache file found for table {table}: {filename}")
-                continue
-            # Load the trie from the pickle file
-            try:
-                with open(filename, 'rb') as f:
-                    trie = pickle.load(f)
-                self.local_cache[table] = trie
-                self.logger.info(f"Loaded IP trie for table {table} with {len(trie)} entries")
-            except Exception as e:
-                self.logger.error(f"Error loading IP trie from {filename}: {e}")
+                self.logger.error("No base URL or cache directory specified for IP cacher")
+    
+    def load_from_url(self, table: str):
+        url = f"{self.base_url}/ip_trie_{table}.pickle"
+        self.logger.info(f"Loading IP trie for table {table} from URL: {url}")
+        try:
+            import requests
+            response = requests.get(url)
+            response.raise_for_status()
+            trie = pickle.loads(response.content)
+            self.local_cache[table] = trie
+            self.logger.info(f"Loaded IP trie for table {table} with {len(trie)} entries")
+        except Exception as e:
+            self.logger.error(f"Error loading IP trie from {url}: {e}") 
+
+    def load_from_file(self, table: str):
+        filename = os.path.join(self.cache_dir, f"ip_trie_{table}.pickle")
+        if os.path.exists(filename):
+            self.logger.info(f"Found IP trie cache file for table {table}: {filename}")
+        else:
+            self.logger.warning(f"No IP trie cache file found for table {table}: {filename}")
+            return
+        # Load the trie from the pickle file
+        try:
+            with open(filename, 'rb') as f:
+                trie = pickle.load(f)
+            self.local_cache[table] = trie
+            self.logger.info(f"Loaded IP trie for table {table} with {len(trie)} entries")
+        except Exception as e:
+            self.logger.error(f"Error loading IP trie from {filename}: {e}")
 
     def lookup(self, table, key: str) -> Optional[str]:
         return self.local_cache.get(table, {}).get(key, None) if key and table else None
